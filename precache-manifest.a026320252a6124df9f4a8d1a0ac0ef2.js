dd982bc8a18ffb598f5014e45937d808257833a7
ghleex <ghleex@gmail.com>
2020-07-07 15:40:24 +0900

    Updates

---
+++ precache-manifest.a026320252a6124df9f4a8d1a0ac0ef2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bade018c0709442deaea5e9d0e43026e",
+    "url": "/starting_react/index.html"
+  },
+  {
+    "revision": "5131334e8dec90601263",
+    "url": "/starting_react/static/css/main.1a0e85ed.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/starting_react/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/starting_react/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5131334e8dec90601263",
+    "url": "/starting_react/static/js/main.ba89ab28.chunk.js"
+  },
+  {
+    "revision": "6f956be3b6366f9dfed3",
+    "url": "/starting_react/static/js/runtime-main.1a26779c.js"
+  }
+]);
